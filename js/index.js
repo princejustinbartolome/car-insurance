3167e5aebbd7e188ec3b5602f985a892a8371248
princejustinbartolome <pjdb.bartolome@gmail.com>
2024-04-27 00:04:55 +0800

    Commit for site Car Insurance

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+$(document).ready(function() {
+
+    $('input[type=number][max]:not([max=""])').on('input', function(ev) {
+      var $this = $(this);
+      var maxlength = $this.attr('max').length;
+      var value = $this.val();
+      if (value && value.length >= maxlength) {
+        $this.val(value.substr(0, maxlength));
+      }
+    });
+  
+  });
